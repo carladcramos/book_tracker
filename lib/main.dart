09545e04d8b5cd1b4f14797ef8402f8fe759c3c5
carladcramos <crlrms28@gmail.com>
2025-06-19 10:22:35 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,36 +7,95 @@ void main() {
         title: Text('Book Tracker'),
         backgroundColor: Colors.lightBlue[800],
       ),
-      body: Center( 
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,  
-          crossAxisAlignment: CrossAxisAlignment.center, 
-          children: [
-            Text(
-              'Text 1',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+      body: Column(
+        children: [
+          // Name Row
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0), // Padding inside the text widget
+                  child: Text(
+                    'Name:',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10), // Padding inside the text widget
+                  child: Text(
+                    'Carla D. Ramos',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 10), 
-            Text(
-              'Text 2',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+          ),
+
+          // Age Row
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0), // Padding inside the text widget
+                  child: Text(
+                    'Age:',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10), // Padding inside the text widget
+                  child: Text(
+                    '21 Years Old',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 10),
-            Text(
-              'Text 3',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+          ),
+
+          // Gender Row
+          Container(
+            margin: EdgeInsets.fromLTRB(5, 4, 3, 2),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0), // Padding inside the text widget
+                  child: Text(
+                    'Gender:',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10), // Padding inside the text widget
+                  child: Text(
+                    'Female',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     ),
   ));
